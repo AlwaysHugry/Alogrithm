850e6c67df74b872cbb6f85a628100ee761a4976
AlwaysHugry <1279324407@qq.com>
2020-03-16 21:53:53 +0800

    重置前提交

--- Luogu/Recrsion/P1498.cpp
+++ Luogu/Recrsion/P1498.cpp
@@ -1,19 +1,16 @@
-/*
-    need to debug;
-*/
 #include<iostream>
 using namespace std;
 char s[3000][3000];//用于存储图形
 void toRight(int n)//向右复制
 {
-    for(int i=0;i<4*n;i++)
-    for(int j=0;j<2*n;j++)
-    s[4*n+i][j]=s[i][j]; 
+    for(int j=0;j<4*n;j++)//每一列
+    for(int i=0;i<2*n;i++)//每一行
+    s[i][4*n+j]=s[i][j];
 } 
 void toUp(int n)//向上复制
 {
-    for(int i=0;i<4*n;i++)
-    for(int j=0;j<n;j++)
+    for(int j=0;j<4*n;j++)
+    for(int i=0;i<2*n;i++)
     s[2*n+i][j+2*n]=s[i][j];
 }
 int main()
@@ -24,9 +21,9 @@ int main()
     int  n;
     int k=1;//用以记录图形规模
     cin>>n;
-    s[0][0]='/';
-    s[0][1]='-';
-    s[0][2]='-';
+    s[0][0]='/';//第一个是行,第二个是列 
+    s[0][1]='_';
+    s[0][2]='_';
     s[0][3]='\\';
     s[1][1]='/';
     s[1][2]='\\';//构造第一个图形,用以递推出更高阶的图形
@@ -39,10 +36,11 @@ int main()
             k*=2;
         }
     }
-    for(int i=2*k-1;i>=0;i--)
+    for(int i=2*k-1;i>=0;i--)//最高行
     {
-        for(int j=0;j<2*k+i;j++)
+        for(int j=0;j<(4*k-i);j++)
         cout<<s[i][j];
         cout<<endl;
+
     }
 }
